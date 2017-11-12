e991b2e5169618bba0884576e6b831ce905d85c0
Lane Bryer <lanebryer@gmail.com>
2017-11-12 13:28:40 +0000

    Fina

--- smallsh.c
+++ smallsh.c
@@ -12,7 +12,7 @@
 #define MAX_PIDS 1000
 typedef enum {false, true} bool;
 
-char enteredLine[INPUT_LENGTH];
+char* enteredLine = NULL;
 char finalLine[10000];
 char* arguments[MAX_ARGS];
 bool outputRedirect;
@@ -25,6 +25,9 @@ int argc;
 pid_t backgroundProcesses[MAX_PIDS];
 int inputFile;
 int outputFile;
+int runningForeground = -5;
+bool foreGroundOnly = false;
+int mostRecentForeground;
 
 
 /************************************************************************
@@ -32,11 +35,18 @@ int outputFile;
 ************************************************************************/
 void getInput()
 {
-    if(fgets(enteredLine, sizeof(enteredLine), stdin) != NULL)
-       {
-            char *newLine = strchr(enteredLine, '\n');
-            *newLine = '\0';
-       }
+    size_t bufferSize;
+    int charCount = -5;
+    charCount = getline(&enteredLine, &bufferSize, stdin);
+    
+    if(charCount == -1)
+    {
+        clearerr(stdin);
+    }
+    else
+    {
+        enteredLine[strcspn(enteredLine, "\n")] = '\0';
+    }
 }
 
 /************************************************************************
@@ -199,7 +209,12 @@ void performRedirection()
                 argc = argc - 2;
                 inputRedirect = true;
             }
+            else
+            {
+                break;
+            }
         }
+        
     
         if (background == false)
         {
@@ -279,7 +294,7 @@ void performRedirection()
 ************************************************************************/
 void expandPids()
 {
-   char final[10000];
+    char final[10000];
     char firstHalf[5000];
     char secondHalf[5000];
     char *ptr = enteredLine;
@@ -293,11 +308,43 @@ void expandPids()
         ptr = ptr + chars + 2;          //increment the ptr by the number of chars in the initial string + the number of chars in the "$$" string
         strcpy(secondHalf, ptr);        //copy the remaining characters after the '$$" to a different variable'
         sprintf(final, "%s%d%s", firstHalf, getpid(), secondHalf); //print a string that consists of: firstHalf + pid + secondHalf
-        printf("%s\n", final);
         ptr = final;                    //ptr set to the expanded string to prep for further while loop iterations
         ptr2 = strstr(final, "$$");     //ptr2 set to the next instance of "$$" for further while loop iteration.
     }
         strcpy(finalLine, final);       //copies the final result to the global variable "finalLine" for use in the main function
+        memset(final,0,sizeof(final));
+        memset(firstHalf,0,sizeof(firstHalf));
+        memset(secondHalf,0,sizeof(secondHalf));
+}
+
+void sendSIGINT_action()
+{
+    if (spawnpid == 0 || runningForeground > 0)
+    {
+        kill(runningForeground, SIGTERM);
+        char* message = "Foreground process terminated by signal 15\n";
+        write(STDOUT_FILENO, message, 43);
+        runningForeground = -5;
+    }
+
+}
+
+void handleSIGTSTP()
+{
+    char* message;
+    if (foreGroundOnly == false)
+    {
+        message = "Entering foreground-only mode (& is now ignored)\n";
+        write(STDOUT_FILENO, message, 49);
+        foreGroundOnly = true;
+    }
+    else
+    {
+        message = "Exiting foreground-only mode\n";
+        write(STDOUT_FILENO, message, 29);
+        foreGroundOnly = false;
+    }
+
 }
 
 
@@ -308,6 +355,18 @@ void main()
     inputRedirect = false;
     int i;
     initializeBackgroundArray();
+    
+    struct sigaction getSIGTSTP = {0};
+    getSIGTSTP.sa_handler = handleSIGTSTP;
+    getSIGTSTP.sa_flags = 0;
+    sigfillset(&(getSIGTSTP.sa_mask));
+    sigaction(SIGTSTP, &getSIGTSTP, NULL);
+    
+    struct sigaction getSIGINT = {0};
+    getSIGINT.sa_handler = sendSIGINT_action;
+    getSIGINT.sa_flags = 0;
+    sigfillset(&(getSIGINT.sa_mask));
+    sigaction(SIGINT, &getSIGINT, NULL);
     
     while(!timeToExit)                                  //loops until the exit command is called
     {
@@ -325,22 +384,40 @@ void main()
         {
             expandPids();
         }
-        
-        if (strcmp(finalLine, "status") == 0)           //Checks for built-in status command
-        {
-            printf("exit value %d\n", statusCode);
-        }
-        else if (strncmp(finalLine, "#", 1) == 0 || strcmp(finalLine, "") == 0)         //Checks for a blank or comment line
+
+        if (strncmp(finalLine, "#", 1) == 0 || strcmp(finalLine, "") == 0)         //Checks for a blank or comment line
         {
             continue;
         }
-        else if (strcmp(finalLine, "exit") == 0)                                        //Checks for exit command
+        
+        tokenizeInput();
+        
+        if (strcmp(arguments[0], "exit") == 0)                                        //Checks for exit command
         {
+            int i;
+            for (i = 0; i < MAX_PIDS; i++)
+            {
+                if(backgroundProcesses[i] != -1)
+                {
+                    kill(backgroundProcesses[i], SIGKILL);
+                }
+            }
             exit(0);
         }
-        else
+        else if (strcmp(arguments[0], "status") == 0)
         {
-            tokenizeInput();                                                            //If not a blank/comment, exit, or status command, tokenize input for cd/exec
+                if (WIFEXITED(statusCode)) //this checks if the process was ended by exiting
+                {
+                    printf("Exit value %d\n", WEXITSTATUS(statusCode));
+                    fflush(stdout);
+                }
+                else if (WIFSIGNALED(statusCode)) //this checks if the process was ended by a signal
+                {
+                    printf("Terminated by signal %d\n", WTERMSIG(statusCode));
+                }
+            }
+        else
+        {                                                            //If not a blank/comment, exit, or status command, tokenize input for cd/exec
             if(strcmp(arguments[0], "cd") == 0)                                         
             {
                 changeDirectory();
@@ -353,7 +430,7 @@ void main()
                 
                 if (spawnpid == -1)                                                     //if spawnpid == -1, an error occurred
                 {
-                    perror("Forking failed!\n");
+                    perror("Forking failed!\n"); fflush(stdout);
                     exit(1);
                 }
                 else if (spawnpid == 0)                                                 //if spawnpid == 0, we are in the child process.  Perform redirection and execute.
@@ -362,19 +439,21 @@ void main()
                     execCode = execvp(arguments[0], arguments);
                     if (execCode == -1)                                                 //if exec returns -1, we have a problem.
                     {
-                        printf("Command could not be executed.\n");
-                        statusCode = 1;
+                        printf("Command could not be executed.\n"); fflush(stdout);
+                        exit(1);
                     }
                 }
                 else                                                                    //if spawnpid != 0 or -1, we are in the parent process (the shell)
                 {
-                    if (background == false)                                            //if this is a foreground process   
+                    if (background == false || foreGroundOnly == true)                                            //if this is a foreground process   
                     {
-                        waitpid(spawnpid, &statusCode, 0);                              //We wait for the process to complete
+                        runningForeground = spawnpid;
+                        mostRecentForeground = waitpid(spawnpid, &statusCode, 0);                              //We wait for the process to complete
+                        runningForeground = -5;
                     }
                     else                                                                //If it is a background process
                     {
-                        printf("background pid is %d\n", spawnpid);                     //Output the process id and
+                        printf("background pid is %d\n", spawnpid); fflush(stdout);   //Output the process id and
                         addPidToArray(spawnpid);                                        //add it to the background process array
                     }
                 }
